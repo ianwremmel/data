4089dc73695f25f3df7e74827b527cb34cbbad03
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2022-11-11 17:54:52 -0800

    refactor: use yml library to format yml

--- src/codegen/cloudformation/plugin.ts
+++ src/codegen/cloudformation/plugin.ts
@@ -6,8 +6,10 @@ import {
   PluginFunction,
 } from '@graphql-codegen/plugin-helpers';
 import {assertObjectType, GraphQLObjectType, isObjectType} from 'graphql';
+import yml from 'js-yaml';
 import {snakeCase} from 'lodash';
 
+import {extractTtlInfo} from '../common/fields';
 import {hasDirective, hasInterface} from '../common/helpers';
 import {extractIndexInfo} from '../common/indexes';
 
@@ -36,200 +38,170 @@ export const plugin: PluginFunction<CloudformationPluginConfig> = (schema) => {
 
     const tableNames = tableTypes.map((type) => type.name).sort();
 
-    // Eventually, this should modifiy an existing file by using Guard comments
+    // Eventually, this should modify an existing file by using Guard comments
     // and adding/replacing sections as relevant, but for now, we'll just do the
     // basic generation to prove the concept
-    return `AWSTemplateFormatVersion: '2010-09-09'
-Transform: 'AWS::Serverless-2016-10-31'
 
-Conditions:
-  IsProd: !Equals
-    - !Ref StageName
-    - production
-
-Globals:
-  Function:
-    Environment:
-      Variables:
-        ### Generated Table Name Environment Variables ###
-${tableNames
-  .map(
-    (tableName) =>
-      `        TABLE_${snakeCase(
-        tableName
-      ).toUpperCase()}: !Ref Table${tableName}`
-  )
-  .join('\n')}
-        ### End Generated Table Name Environment Variables ###
-
-Outputs:
-  ### Generated Table Name Outputs ###
-${tableNames
-  .map(
-    (typeName) => `  Table${typeName}:
-    Description: The name of the DynamoDB table for ${typeName}
-    Value: !Ref Table${typeName}
-    Export:
-      Name: !Sub \${AWS::StackName}-Table${typeName}
-      Value: !Ref Table${typeName}`
-  )
-  .join('\n')}
-  ### End Generated Table Name Outputs ###
-
-Parameters:
-  StageName:
-    Type: String
-    Description: The name of the stage
-    AllowedValues:
-      - development
-      - production
-      - test
-    Default: development
-
-Resources:
-  ### Generated Table Resources ###
-${tableTypes
-  .map((type) => {
-    const tableName = `Table${type.name}`;
-
-    assertObjectType(type);
-    // Again, I don't know why I need to cast this.
-    const isCompositeKey = hasDirective(`compositeKey`, type);
-
-    const attributeDefinitions = isCompositeKey
-      ? [
-          {
-            AttributeName: 'pk',
-            AttributeType: 'S',
+    const tpl = {
+      AWSTemplateFormatVersion: '2010-09-09',
+      Transform: 'AWS::Serverless-2016-10-31',
+      // eslint-disable-next-line sort-keys
+      Conditions: {
+        IsProd: {'Fn::Equals': [{Ref: 'StageName'}, 'production']},
+      },
+      Globals: {
+        Function: {
+          Environment: {
+            Variables: tableNames.reduce((acc, tableName) => {
+              acc[`TABLE_${snakeCase(tableName).toUpperCase()}`] = {
+                Ref: `Table${tableName}`,
+              };
+              return acc;
+            }, {} as Record<string, {Ref: string}>),
           },
-          {
-            AttributeName: 'sk',
-            AttributeType: 'S',
-          },
-        ]
-      : [{AttributeName: 'id', AttributeType: 'S'}];
-
-    const keySchema = isCompositeKey
-      ? [
-          {
-            AttributeName: 'pk',
-            KeyType: 'HASH',
-          },
-          {
-            AttributeName: 'sk',
-            KeyType: 'RANGE',
-          },
-        ]
-      : [
-          {
-            AttributeName: 'id',
-            KeyType: 'HASH',
-          },
-        ];
-
-    const globalSecondaryIndexes = [];
-
-    const indexInfo = extractIndexInfo(type);
-    for (const index of indexInfo.indexes) {
-      attributeDefinitions.push(
-        {
-          AttributeName: `${index.name}pk`,
-          AttributeType: 'S',
         },
-        {
-          AttributeName: `${index.name}sk`,
-          AttributeType: 'S',
+      },
+      Outputs: tableNames.reduce((acc, name) => {
+        const tableName = `Table${name}`;
+        acc[tableName] = {
+          Description: `The name of the DynamoDB table for ${name}`,
+          Export: {
+            Name: {'Fn::Sub': `\${AWS::StackName}-${tableName}`},
+            Value: {Ref: tableName},
+          },
+          Value: {Ref: tableName},
+        };
+        return acc;
+      }, {} as Record<string, object>),
+      Parameters: {
+        StageName: {
+          Type: 'String',
+          // eslint-disable-next-line sort-keys
+          AllowedValues: ['development', 'production', 'test'],
+          Description: 'The name of the stage',
+          // eslint-disable-next-line sort-keys
+          Default: 'development',
+        },
+      },
+      Resources: tableTypes.reduce((acc, type) => {
+        const tableName = `Table${type.name}`;
+
+        assertObjectType(type);
+        const isCompositeKey = hasDirective(`compositeKey`, type);
+        const indexInfo = extractIndexInfo(type);
+        const ttlInfo = extractTtlInfo(type);
+
+        const attributeDefinitions = isCompositeKey
+          ? [
+              {
+                AttributeName: 'pk',
+                AttributeType: 'S',
+              },
+              {
+                AttributeName: 'sk',
+                AttributeType: 'S',
+              },
+            ]
+          : [{AttributeName: 'id', AttributeType: 'S'}];
+
+        const keySchema = isCompositeKey
+          ? [
+              {
+                AttributeName: 'pk',
+                KeyType: 'HASH',
+              },
+              {
+                AttributeName: 'sk',
+                KeyType: 'RANGE',
+              },
+            ]
+          : [
+              {
+                AttributeName: 'id',
+                KeyType: 'HASH',
+              },
+            ];
+
+        const globalSecondaryIndexes = [];
+        for (const index of indexInfo.indexes) {
+          attributeDefinitions.push(
+            {
+              AttributeName: `${index.name}pk`,
+              AttributeType: 'S',
+            },
+            {
+              AttributeName: `${index.name}sk`,
+              AttributeType: 'S',
+            }
+          );
+
+          globalSecondaryIndexes.push({
+            IndexName: index.name,
+            KeySchema: [
+              {
+                AttributeName: `${index.name}pk`,
+                KeyType: 'HASH',
+              },
+              {
+                AttributeName: `${index.name}sk`,
+                KeyType: 'RANGE',
+              },
+            ],
+            Projection: {
+              ProjectionType: 'ALL',
+            },
+          });
         }
-      );
 
-      globalSecondaryIndexes.push({
-        IndexName: index.name,
-        KeySchema: [
-          {
-            AttributeName: `${index.name}pk`,
-            KeyType: 'HASH',
+        const properties: Record<string, unknown> = {
+          AttributeDefinitions: attributeDefinitions,
+          BillingMode: 'PAY_PER_REQUEST',
+          GlobalSecondaryIndexes: globalSecondaryIndexes,
+          KeySchema: keySchema,
+          PointInTimeRecoverySpecification: {
+            PointInTimeRecoveryEnabled: true,
           },
-          {
-            AttributeName: `${index.name}sk`,
-            KeyType: 'RANGE',
+          SSESpecification: {
+            SSEEnabled: {'Fn::If': ['IsProd', true, false]},
           },
-        ],
-      });
-    }
+          StreamSpecification: {
+            StreamViewType: 'NEW_AND_OLD_IMAGES',
+          },
+          Tags: [
+            {
+              Key: 'StageName',
+              Value: {Ref: 'StageName'},
+            },
+            {
+              Key: 'TableName',
+              Value: tableName,
+            },
+          ],
+          TimeToLiveSpecification: {
+            AttributeName: 'ttl',
+            Enabled: true,
+          },
+        };
 
-    const ttlField =
-      type.astNode?.kind === 'ObjectTypeDefinition' &&
-      type.astNode.fields?.find((field) =>
-        field.directives?.map(({name}) => name.value).includes('ttl')
-      )?.name.value;
+        if (globalSecondaryIndexes.length === 0) {
+          delete properties.GlobalSecondaryIndexes;
+        }
 
-    return `
-  ${tableName}:
-    Type: AWS::DynamoDB::Table
-    Properties:
-      AttributeDefinitions:
-${attributeDefinitions
-  .map(
-    ({
-      AttributeName,
-      AttributeType,
-    }) => `        - AttributeName: ${AttributeName}
-          AttributeType: ${AttributeType}`
-  )
-  .join('\n')}
-      BillingMode: PAY_PER_REQUEST${
-        globalSecondaryIndexes.length > 0
-          ? `
-      GlobalSecondaryIndexes:
-${globalSecondaryIndexes
-  .map(
-    (gsi) => `        - IndexName: ${gsi.IndexName}
-          KeySchema:
-${gsi.KeySchema.map(
-  (ks) => `            - AttributeName: ${ks.AttributeName}
-              KeyType: ${ks.KeyType}`
-).join('\n')}
-          Projection:
-            ProjectionType: ALL`
-  )
-  .join('\n')}`
-          : ''
-      }
-      KeySchema:
-${keySchema
-  .map(
-    ({AttributeName, KeyType}) => `        - AttributeName: ${AttributeName}
-          KeyType: ${KeyType}`
-  )
-  .join('\n')}
-      PointInTimeRecoverySpecification:
-        PointInTimeRecoveryEnabled: true
-      SSESpecification:
-        # Use the customer-owned, AWS-managed KMS key in prod; use the
-        # AWS-owned, AWS-managed KMS key in dev and test.
-        SSEEnabled: !If
-          - IsProd
-          - true
-          - false
-      StreamSpecification:
-        StreamViewType: NEW_AND_OLD_IMAGES
-      Tags:
-        - Key: StageName
-          Value: !Ref StageName
-        - Key: TableName
-          Value: ${tableName}
-      ${
-        ttlField
-          ? `TimeToLiveSpecification:
-          AttributeName: ttl
-          Enabled: true`
-          : ''
-      }`;
-  })
-  .join('\n')}
+        if (!ttlInfo) {
+          delete properties.TimeToLiveSpecification;
+        }
 
-  ### End Generated Table Resources ###
-    `;
+        acc[tableName] = {
+          Type: 'AWS::DynamoDB::Table',
+          // eslint-disable-next-line sort-keys
+          Properties: properties,
+        };
+
+        return acc;
+      }, {} as Record<string, object>),
+    };
+
+    return yml.dump(tpl);
   } catch (err) {
     // graphql-codegen suppresses stack traces, so we have to re-log here.
     console.error(err);
