649ae61237d099e55a02ea03e824aec036cf7752
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2022-12-14 08:55:14 -0800

    refactor(actions): make ensureTable unaware of GraphQL

--- src/codegen/actions/tables/templates/ensure-table.ts
+++ src/codegen/actions/tables/templates/ensure-table.ts
@@ -1,21 +1,10 @@
-import type {GraphQLObjectType} from 'graphql';
 import {snakeCase} from 'lodash';
 
 /**
  * Generates the code for checking that the environment variables for this
  * table's name has been set.
  */
-export function ensureTableTemplate(
-  objType: GraphQLObjectType | string
-): string {
-  if (typeof objType === 'string') {
-    return `  const tableName = process.env.${snakeCase(objType).toUpperCase()};
+export function ensureTableTemplate(objType: string): string {
+  return `  const tableName = process.env.${snakeCase(objType).toUpperCase()};
   assert(tableName, '${snakeCase(objType).toUpperCase()} is not set');`;
-  }
-  return `  const tableName = process.env.TABLE_${snakeCase(
-    objType.name
-  ).toUpperCase()};
-  assert(tableName, 'TABLE_${snakeCase(
-    objType.name
-  ).toUpperCase()} is not set');`;
 }
